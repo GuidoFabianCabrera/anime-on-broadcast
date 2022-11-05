af4af7d3e6a55fc75f25b340cfc1302e8032d131
GuidoFC <guidofabiancabrera@hotmail.com>
2022-11-05 02:30:14 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { makeStyles } from '@mui/styles';
+import _ from 'underscore';
 
 import { ExampleContext } from '../store/example';
 
-import data from '../data.json';
-import { Grid } from '@mui/material';
+import { Button, Container, Grid } from '@mui/material';
+
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   box: {
@@ -16,17 +18,56 @@ const Home = () => {
   const classes = useStyles();
   const { test } = useContext(ExampleContext);
 
-  return (
-    <Grid container spacing={3}>
-      {data.map((item, index) => (
-        <Grid item xs={3} key={index}>
-          <div>{item.name}</div>
-          <img src={item.image} />
-          <div>{item.date}</div>
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getAnimes();
+  }, []);
+
+  const getAnimes = () => {
+    axios.get('https://anime-on-broadcast.herokuapp.com/animes').then(
+      (result) => {
+        setIsLoaded(true);
+
+        const alldates = _.groupBy(result.data, function (obj) {
+          return obj.date;
+        });
+
+        setItems(alldates);
+      },
+
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
+  };
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          {Object.keys(items).map((keyName, i) => (
+            <li className="travelcompany-input" key={i}>
+              {items[keyName].map((item, index) => (
+                <Grid item key={index} style={{ maxWidth: '230px' }}>
+                  <div>{item.name}</div>
+                  <img src={item.image.url} width="100%" />
+                  <div>{item.date}</div>
+                </Grid>
+              ))}
+            </li>
+          ))}
         </Grid>
-      ))}
-    </Grid>
-  );
+      </Container>
+    );
+  }
 };
 
 export default Home;
